b8e40b8bca94e43d61c947943cb403d3b3b84d19
Lucas Balieiro <lucasbalieirom@hotmail.com>
2023-01-06 13:26:32 -0400

    feat: add bootstrap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,11 @@
-import { Checkbox, Container, FormControlLabel, FormGroup, FormLabel, TextField } from "@mui/material"
-import LinkButton from "../component/LinkButton"
-import TitleAndSubHeader from "../component/TitleAndSubHeader"
+import { Container } from "react-bootstrap";
+
 
 export default function Home() {
-    return (
-        <Container maxWidth="sm" style={
-            {
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100vh"
-            }
-        }>
-            <TitleAndSubHeader title="Cadastrar-se para Escala VIPM" subHeader="Fevereiro" />
-            <LinkButton href="/listagem" text="Listagem" colorStyle="secondary" />
-
-            <TextField name="name" label="Seu nome" variant="filled" />
-            <br/>
-            <FormGroup>
-                <FormLabel component="legend">Disponibilidades</FormLabel>
-                <FormControlLabel control={<Checkbox />} label="Domingo - 05/02/2023" />
-                <FormControlLabel control={<Checkbox />} label="Domingo - 12/02/2023" />
-                <FormControlLabel control={<Checkbox />} label="Domingo - 19/02/2023" />
-                <FormControlLabel control={<Checkbox />} label="Domingo - 26/02/2023" />
-                <FormControlLabel control={<Checkbox />} label="Aniversario V IPM - 25/02/2023" />
-
-            </FormGroup>
-
-            <LinkButton href="/" text="Cadastrar" colorStyle="primary" />
-            
-
-        </Container>
-    )
+  return (
+    <Container>
+      <h1>Home</h1>
+      <p>Some content</p>
+    </Container>
+  );
 }
