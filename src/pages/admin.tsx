5adc505b70011f5171dd82aca4bc13f770936c3d
Lucas Balieiro <lucasbalieirom@hotmail.com>
2023-01-29 15:48:39 -0400

    feat: add password request to delete all data

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 
@@ -5,14 +6,22 @@ import { Button, Container, Form } from "react-bootstrap";
 export default function Admin() {
 
     const [disabled, setDisabled] = useState(false)
+    const [password, setPassword] = useState("")
 
     async function handleClearAllData(event: any) {
         event.preventDefault();
+
+        if (password !== "ff44f4d00355e63175f7d53a194ce2a4") {
+            alert("Senha incorreta!")
+            return
+        }
+
+
         setDisabled(true)
 
-        const response = await fetch("/api/clearAllData", {
-            method: "DELETE",
-        });
+        const response = await axios.delete("/api/clearAllData", { data: { password } })
+
+        response.status === 401 ? alert("Você não tem permissão para apagar os dados!") : null;
 
         response.status === 200 ? alert("Dados apagados com sucesso!") : alert("Erro ao apagar os dados!");
         setDisabled(false)
@@ -25,6 +34,13 @@ export default function Admin() {
             >
                 <Form.Group className="mb-3">
                     <Form.Label>Deseja limpar todos os dados?</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Digite sua senha"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                    />
                     <Button 
                         variant="primary" 
                         type="submit"
--- src/pages/api/clearAllData.ts
+++ src/pages/api/clearAllData.ts
@@ -27,6 +27,11 @@ async function connectToDatabase(uri: string | undefined) {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'DELETE') {
+        const { password } = req.body
+        if (password !== "ff44f4d00355e63175f7d53a194ce2a4") {
+            res.status(401).json({ message: 'Not authorized!' })
+            return
+        }
         const db = await connectToDatabase(process.env.MONGODB_URI)
         const subscribersCollection = db.collection('subscribers')
         const eventsCollection = db.collection('events')
